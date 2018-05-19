bddede0b6115fe5ca252a642617b211dcdfb54b9
Stephen Paul Weber <singpolyma@singpolyma.net>
2018-05-19 14:11:57 -0500

    Qualified import of Control.Exception for tests

--- tests/suite.hs
+++ tests/suite.hs
@@ -5,13 +5,13 @@ import Test.HUnit hiding (Test, assert)
 
 import Data.Typeable (Typeable)
 import Control.Monad
-import Control.Exception as Ex
 import System.Exit
+import qualified Control.Exception as Ex
 
 import qualified UnexceptionalIO as UIO
 
 data CustomException = CustomException deriving (Show, Typeable)
-instance Exception CustomException
+instance Ex.Exception CustomException
 
 class TestClass a where
 	testClassMethod :: a -> ()
@@ -31,9 +31,9 @@ fromIOCatches io = do
 
 fromIOPasses :: IO () -> Assertion
 fromIOPasses io = do
-	caught <- try $ UIO.run $ UIO.fromIO io
+	caught <- Ex.try $ UIO.run $ UIO.fromIO io
 	either
-		(\(SomeException _) -> return ())
+		(\(Ex.SomeException _) -> return ())
 		(\x -> assertFailure $ "fromIO caught: " ++ show x)
 		caught
 
@@ -42,20 +42,20 @@ tests =
 	[
 		testGroup "fromIO catches runtime errors" [
 			testCase "fail" (fromIOCatches $ fail "boo"),
-			testCase "userError" (fromIOCatches $ throwIO $ userError "boo"),
-			testCase "CustomException" (fromIOCatches $ throwIO CustomException)
+			testCase "userError" (fromIOCatches $ Ex.throwIO $ userError "boo"),
+			testCase "CustomException" (fromIOCatches $ Ex.throwIO CustomException)
 		],
 		testGroup "fromIO passes through programmer errors" [
 #if MIN_VERSION_base(4,9,0)
-			testCase "TypeError" (fromIOPasses $ throwIO $ Ex.TypeError "boo"),
+			testCase "TypeError" (fromIOPasses $ Ex.throwIO $ Ex.TypeError "boo"),
 #endif
 			testCase "error" (fromIOPasses $ error "boo"),
 			testCase "undefined" (fromIOPasses undefined),
 			testCase "ArithException" (fromIOPasses $ void (return $! 1 `div` 0)),
-			testCase "assert" (fromIOPasses $ assert False (return ())),
+			testCase "assert" (fromIOPasses $ Ex.assert False (return ())),
 			testCase "pattern match fail" (fromIOPasses $ (\(Just x) -> return ()) Nothing),
-			testCase "array out of bounds" (fromIOPasses $ throwIO $ IndexOutOfBounds "boo"),
-			testCase "array uninitialized" (fromIOPasses $ throwIO $ UndefinedElement "boo"),
+			testCase "array out of bounds" (fromIOPasses $ Ex.throwIO $ Ex.IndexOutOfBounds "boo"),
+			testCase "array uninitialized" (fromIOPasses $ Ex.throwIO $ Ex.UndefinedElement "boo"),
 			testCase "no method" (fromIOPasses $ print $ testClassMethod CantTestClass),
 			testCase "use uninitialized record field" (fromIOPasses $ print $ badfld BadRecord {}),
 			testCase "use not present record field" (fromIOPasses $ print $ otherfld BadRecord {}),
@@ -68,19 +68,22 @@ tests =
 			testCase "exitSuccess" (fromIOPasses exitSuccess),
 			testCase "exitFailure" (fromIOPasses exitFailure)
 		],
-		testGroup "fromIO passes through asynchronous exceptions from the runtime" [
+		testGroup "fromIO passes through exceptions from the runtime" [
 #if MIN_VERSION_base(4,8,0)
-			testCase "AllocationLimitExceeded" (fromIOPasses $ throwIO Ex.AllocationLimitExceeded),
+			testCase "AllocationLimitExceeded" (fromIOPasses $ Ex.throwIO Ex.AllocationLimitExceeded),
 #endif
-			testCase "NonTermination" (fromIOPasses $ throwIO Ex.NonTermination),
-			testCase "StackOverflow" (fromIOPasses $ throwIO Ex.StackOverflow),
-			testCase "HeapOverflow" (fromIOPasses $ throwIO Ex.HeapOverflow),
-			testCase "ThreadKilled" (fromIOPasses $ throwIO Ex.ThreadKilled),
-			testCase "UserInterrupt" (fromIOPasses $ throwIO Ex.UserInterrupt),
-			testCase "BlockedIndefinitelyOnMVar" (fromIOPasses $ throwIO Ex.BlockedIndefinitelyOnMVar),
-			testCase "BlockedIndefinitelyOnSTM" (fromIOPasses $ throwIO Ex.BlockedIndefinitelyOnSTM),
-			testCase "Deadlock" (fromIOPasses $ throwIO Ex.Deadlock),
-			testCase "NestedAtomically" (fromIOPasses $ throwIO Ex.NestedAtomically)
+#if MIN_VERSION_base(4,7,0)
+			testCase "ChildThreadError" (fromIOPasses $ Ex.throwIO $ UIO.ChildThreadError $ UIO.ProgrammerError $ UIO.ArithException Ex.DivideByZero),
+#endif
+			testCase "NonTermination" (fromIOPasses $ Ex.throwIO Ex.NonTermination),
+			testCase "StackOverflow" (fromIOPasses $ Ex.throwIO Ex.StackOverflow),
+			testCase "HeapOverflow" (fromIOPasses $ Ex.throwIO Ex.HeapOverflow),
+			testCase "ThreadKilled" (fromIOPasses $ Ex.throwIO Ex.ThreadKilled),
+			testCase "UserInterrupt" (fromIOPasses $ Ex.throwIO Ex.UserInterrupt),
+			testCase "BlockedIndefinitelyOnMVar" (fromIOPasses $ Ex.throwIO Ex.BlockedIndefinitelyOnMVar),
+			testCase "BlockedIndefinitelyOnSTM" (fromIOPasses $ Ex.throwIO Ex.BlockedIndefinitelyOnSTM),
+			testCase "Deadlock" (fromIOPasses $ Ex.throwIO Ex.Deadlock),
+			testCase "NestedAtomically" (fromIOPasses $ Ex.throwIO Ex.NestedAtomically)
 		]
 	]
 
